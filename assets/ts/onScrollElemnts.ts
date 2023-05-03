81746eb13ded125cac4861959db4120f549d1e97
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-05-03 15:36:52 -0300

    Update sidebar single

--- assets/ts/onScrollElemnts.ts
+++ assets/ts/onScrollElemnts.ts
@@ -12,19 +12,17 @@ async function setSideBarOnScroll() {
 	// Sidebar Sticky
 	let sidebarContent = document.getElementsByClassName('sidebar-single')[0];
 
-	let scrollTop = window.scrollY + 200;
+	let scrollTop = window.scrollY;
 	let fixHeight =
 		sidebarContent.getBoundingClientRect().height - window.innerHeight;
 
-	if (scrollTop >= fixHeight) {
-		sidebarContent.style.top = `100px`;
+	if (scrollTop - 200 >= fixHeight) {
+		sidebarContent.style.top = `-20px`;
 		sidebarContent.style.position = 'fixed';
-		sidebarContent.style.paddingRight = '200px';
-		sidebarContent.style.marginRight = '200px';
+		sidebarContent.style.marginRight = '100px';
 	} else {
 		sidebarContent.style.top = '';
 		sidebarContent.style.position = '';
-		sidebarContent.style.paddingRight = '';
 		sidebarContent.style.marginRight = '';
 	}
 }
